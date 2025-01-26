c4b4bd1219422ad800f18d16a8eb1aa09a72eae6
user <user@ERP-SOFT3>
2025-01-26 11:54:13 +0600

    Add project files.

---
+++ People_Power/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace People_Power.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
